3323fb4a6afd0f4f042bac0854a7471b5f938810
Daniela Karmova <daniela.krmv@abv.bg>
2024-03-13 16:20:12 +0200

    Added signals

---
+++ accounts/models.py
@@ -0,0 +1,125 @@
+import re
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db.models.fields.related import OneToOneField
+
+# Base User Manager will allow to edit the way how the users and super users are created.
+class UserManager(BaseUserManager):
+
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise ValueError('Password must be at least 8 characters long!')
+        if not re.search(r'\d', password):
+            raise ValueError('Password must contain at least one digit!')
+        if not re.search(r'[A-Z]', password):
+            raise ValueError('Password must contain at least one uppercase letter!')
+        if not re.search(r'[a-z]', password):
+            raise ValueError('Password must contain at least one lowercase letter!')
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            raise ValueError('Password must contain at least one special character!')
+
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email!')
+        
+        if not username:
+            raise ValueError('User must have an username!')
+        
+        self.validate_password(password)
+        
+        user = self.model(
+            # normalize email it will take the email address from me and if this address is with uppercase letters it will convert them to lowercase.
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+
+        user.set_password(password)
+
+        # Django by default uses using parameter to define which database the manager should use for the operations.
+        # this is in cases if we have multiple databases
+        # in this case I have only one database and self._db will take the default database from the config.
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, first_name, last_name, username, email, password=None):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            password = password,
+        )
+
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+
+        return user
+
+
+# with the Abstract Base User we have full control of editing the whole custom user model, including the authentication functionality
+class User(AbstractBaseUser):
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
+
+    # required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+
+class UserProfile(models.Model):
+    # one user can have only one user profile - if we want many profiles (use ForeignKey)
+    # on_delete option means when the user is deleted his profile also should be deleted
+    user = OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=4, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
