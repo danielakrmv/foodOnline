d86a5396c836920a8e1cd080e28b7fd4a65cf7d6
Daniela Karmova <daniela.krmv@abv.bg>
2024-03-24 22:38:10 +0200

    customer and vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -103,6 +103,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
     
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
+    
 
 class UserProfile(models.Model):
     # one user can have only one user profile - if we want many profiles (use ForeignKey)
