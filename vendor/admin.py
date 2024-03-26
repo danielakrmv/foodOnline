22d54956024d0fae34222f78120b32582c57cdf8
Daniela Karmova <daniela.krmv@abv.bg>
2024-03-26 15:34:59 +0200

    vendor and customer dashboard edits

--- vendor/admin.py
+++ vendor/admin.py
@@ -5,7 +5,7 @@ from vendor.models import Vendor
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
     list_display_links = ('user', 'vendor_name')
-    # list_editable = ('is_approved',)
+    list_editable = ('is_approved',)
 
 
 # class OpeningHourAdmin(admin.ModelAdmin):
